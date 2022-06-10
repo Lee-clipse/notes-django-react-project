9869d85664398ed3d0bf445dddbb0e590835adfe
edac99 <79911816+edac99@users.noreply.github.com>
2022-06-10 22:06:34 +0900

    Dev) Changed template path for heroku again

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'frontend')
+            os.path.join(BASE_DIR, 'build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
